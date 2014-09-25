9e379953edf2de6b8b36ee2b299e2ebb0b4a391b
Caesar Wirth <cjwirth@gmail.com>
2014-09-25 12:45:05 +0900

    work maybe?

---
+++ app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/user/{name:[a-z]+}/profile", profile).Methods("GET")
+
+	http.Handle("/", router)
+
+	http.ListenAndServe(":9000", nil)
+}
+
+func profile(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["name"]
+	w.Write([]byte("Hello " + name))
+}
